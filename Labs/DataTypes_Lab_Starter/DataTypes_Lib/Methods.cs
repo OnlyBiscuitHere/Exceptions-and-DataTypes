0721d4be17f279031dfc88d7a4f34e90fc55d165
Benjamin <h8bmf@hotmail.co.uk>
2021-08-01 16:20:24 +0100

    Completed

---
+++ Labs/DataTypes_Lab_Starter/DataTypes_Lib/Methods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DataTypes_Lib
+{
+    public static class Methods
+    {
+        // write a method to return the product of all numbers from 1 to n inclusive
+        public static int Factorial(int n)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+        public static float Mult(float num1, float num2)
+        {
+            float value = (float)(decimal)( num1 * num2);
+            return (float)Math.Round(value,1);
+        }
+    }
+}
---
+++ Labs/DataTypes_Lab_Starter/DataTypes_Lib/TypeConversion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DataTypes_Lib
+{
+    public class TypeConversion
+    {
+        public static short UIntToShort(uint num)
+        {
+            if (num == short.MaxValue + 1)
+            {
+                throw new OverflowException();
+            }
+            else
+            {
+                return (short)num;
+            }
+        }
+
+        public static long FloatToLong(float num)
+        {
+            long value = Convert.ToInt64(num);
+            return value;
+        }
+    }
+}
